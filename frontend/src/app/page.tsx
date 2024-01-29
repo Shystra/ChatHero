58915ffa3869db83dd82ded7351b97b93eb3ba75
Shystra <jlsp.2019@hotmail.com>
2024-01-29 12:45:48 -0300

    style: just first page. I create a Header and a Container to images

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
+import Header from '@/components/Header';
+import Container from "@/components/Container";
 
 export default function Home() {
   return (
     <div>
-      <h1>Test</h1>
+      <Header />
+        <Container>
+          <h1>Test</h1>
+        </Container>
     </div>
   );
 }
