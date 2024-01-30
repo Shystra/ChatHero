1ed7ffac6d9d969bd6ea92c3c1b815565d482edc
Shystra <jlsp.2019@hotmail.com>
2024-01-29 22:21:56 -0300

    feate: creating new components: input and button

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,14 +1,25 @@
 import Image from "next/image";
 import Header from '@/components/Header';
 import Container from "@/components/Container";
+import { Input } from "@/components/Input";
 
 export default function Home() {
   return (
-    <div>
+    <main className="min-h-screen flex flex-col">
       <Header />
+      <div className="max-w-[580px] mx-auto w-full h-full flex flex-1 items-center justify-center">
         <Container>
-          <h1>Test</h1>
+          <div>
+            <span>Ingressar</span>
+            <span>Nova reunião</span>
+          </div>
+          <div>
+          <Input placeholder="Digite o código da reunião" type="text" />
+          <Input placeholder="Digite o código da reunião" type="text" />
+          <button>Entrar</button>
+          </div>
         </Container>
-    </div>
+        </div>
+    </main>
   );
 }
